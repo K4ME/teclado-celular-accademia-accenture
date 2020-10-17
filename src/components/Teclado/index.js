0d2a70056e2a13d3be06b5591db7c6e545b447f8
Marcílio Júnior <jrsmarcilio@gmail.com>
2020-10-17 17:15:32 -0300

    Adicionando as letras no teclado numérico.

--- src/components/Teclado/index.js
+++ src/components/Teclado/index.js
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 import './index.css';
 
 function Teclado() {
+  const [nums, setNums] = useState("");
+
+  useEffect(() => {
+    console.log(typeof(nums));
+    console.log(nums);
+
+  }, nums);
+
+  function ConcatNum(e) {
+    if(nums === '') {
+      setNums(e.value);
+    } else {
+      setNums(`${nums}2`);
+    }
+  }
+
   return (    
       <div className='container'>
         <div className='text-area-container'>
           <textarea></textarea>
         </div>
         <div className='grupo1-container'>
-          <button><sup className="sup-numeros">1</sup></button>
-          <button><sup className="sup-numeros">2</sup> abc</button>
-          <button>def <sup className="sup-numeros">3</sup></button>
+          <button /* onClick={() => setNums(nums.concat('1'))} */>1</button>
+          <button onClick={() => ConcatNum('2')}>
+          <sup className="sup-number">2</sup> abc
+          </button>
+          <button onClick={() => setNums(nums.concat("3"))}>
+          def <sup className="sup-number">3</sup>
+          </button>
         </div>
         <div className='grupo2-container'>
-        <button><sup className="sup-numeros">4</sup> ghi</button>
-          <button><sup className="sup-numeros">5</sup> jkl</button>
-          <button>mno <sup className="sup-numeros">6</sup></button>
+          <button onClick={() => setNums(nums.concat("4"))}>
+            <sup className="sup-number">4</sup> ghi
+          </button>
+          <button onClick={() => setNums(nums.concat("5"))}>
+            <sup className="sup-number">5</sup> jkl
+          </button>
+          <button onClick={() => setNums(nums.concat("6"))}>
+            mno <sup className="sup-number">6</sup>
+          </button>
         </div>
         <div className='grupo3-container'>
-        <button><sup className="sup-numeros">7</sup> pqrs</button>
-          <button><sup className="sup-numeros">8</sup> tuv</button>
-          <button>wxyz <sup className="sup-numeros">9</sup></button>
+          <button onClick={() => setNums(nums.concat("7"))}>
+            <sup className="sup-numeros">7</sup> pqrs
+          </button>
+          <button onClick={() => setNums(nums.concat("8"))}>
+              <sup className="sup-numeros">8</sup> tuv
+          </button>
+          <button onClick={() => setNums(nums.concat("9"))}>
+            wxyz <sup className="sup-numeros">9</sup>
+          </button>
         </div>
       </div>    
   );
