da41cf1ca10b72480b31e5b13a29abcae9c06844
Marcílio Júnior <jrsmarcilio@gmail.com>
2020-10-17 16:41:09 -0300

    Adicionando as letras nos botões de números

--- src/components/Teclado/index.js
+++ src/components/Teclado/index.js
@@ -9,19 +9,19 @@ function Teclado() {
           <textarea></textarea>
         </div>
         <div className='grupo1-container'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button><sup className="sup-numeros">1</sup></button>
+          <button><sup className="sup-numeros">2</sup> abc</button>
+          <button>def <sup className="sup-numeros">3</sup></button>
         </div>
         <div className='grupo2-container'>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+        <button><sup className="sup-numeros">4</sup> ghi</button>
+          <button><sup className="sup-numeros">5</sup> jkl</button>
+          <button>mno <sup className="sup-numeros">6</sup></button>
         </div>
         <div className='grupo3-container'>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+        <button><sup className="sup-numeros">7</sup> pqrs</button>
+          <button><sup className="sup-numeros">8</sup> tuv</button>
+          <button>wxyz <sup className="sup-numeros">9</sup></button>
         </div>
       </div>    
   );
