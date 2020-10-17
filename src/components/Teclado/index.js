6d6ed8c02b18ea3d6cf71e33cffbfe8d93025c4e
viniciusACoelho <viniciuskj.vc@gmail.com>
2020-10-17 16:33:06 -0300

    Conexão com back e useState

--- src/components/Teclado/index.js
+++ src/components/Teclado/index.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import './index.css';
+import "./index.css";
 
 function Teclado() {
-  return (    
-      <div className='container'>
-        <div className='text-area-container'>
-          <textarea></textarea>
-        </div>
-        <div className='grupo1-container'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-        </div>
-        <div className='grupo2-container'>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-        </div>
-        <div className='grupo3-container'>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-        </div>
-      </div>    
+  const [nums, setNums] = useState("");
+
+  useEffect(() => {
+    console.log(nums);
+  }, nums);
+  return (
+    <div className="container">
+      <div className="text-area-container">
+        <textarea></textarea>
+      </div>
+      <div className="grupo1-container">
+        <button /* onClick={() => setNums(nums.concat('1'))} */>1</button>
+        <button onClick={() => setNums(nums.concat("2"))}>2</button>
+        <button onClick={() => setNums(nums.concat("3"))}>3</button>
+      </div>
+      <div className="grupo2-container">
+        <button onClick={() => setNums(nums.concat("4"))}>4</button>
+        <button onClick={() => setNums(nums.concat("5"))}>5</button>
+        <button onClick={() => setNums(nums.concat("6"))}>6</button>
+      </div>
+      <div className="grupo3-container">
+        <button onClick={() => setNums(nums.concat("7"))}>7</button>
+        <button onClick={() => setNums(nums.concat("8"))}>8</button>
+        <button onClick={() => setNums(nums.concat("9"))}>9</button>
+      </div>
+    </div>
   );
 }
 
