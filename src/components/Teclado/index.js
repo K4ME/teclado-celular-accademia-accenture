a25b7c287539cabf16668df501619dde45f0db91
viniciusACoelho <viniciuskj.vc@gmail.com>
2020-10-17 17:24:31 -0300

    concatenação e html/css feito por Marcilio

--- src/components/Teclado/index.js
+++ src/components/Teclado/index.js
@@ -3,32 +3,46 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 
 function Teclado() {
-  const [nums, setNums] = useState("");
+  const [nums, setNums] = useState('');
 
-  useEffect(() => {
-    console.log(nums);
-  }, nums);
-  return (
-    <div className="container">
-      <div className="text-area-container">
-        <textarea></textarea>
-      </div>
-      <div className="grupo1-container">
-        <button /* onClick={() => setNums(nums.concat('1'))} */>1</button>
-        <button onClick={() => setNums(nums.concat("2"))}>2</button>
-        <button onClick={() => setNums(nums.concat("3"))}>3</button>
-      </div>
-      <div className="grupo2-container">
-        <button onClick={() => setNums(nums.concat("4"))}>4</button>
-        <button onClick={() => setNums(nums.concat("5"))}>5</button>
-        <button onClick={() => setNums(nums.concat("6"))}>6</button>
-      </div>
-      <div className="grupo3-container">
-        <button onClick={() => setNums(nums.concat("7"))}>7</button>
-        <button onClick={() => setNums(nums.concat("8"))}>8</button>
-        <button onClick={() => setNums(nums.concat("9"))}>9</button>
-      </div>
-    </div>
+  
+  return (    
+      <div className='container'>
+        <div className='text-area-container'>
+          <textarea></textarea>
+        </div>
+        <div className='grupo1-container'>
+          <button /* onClick={() => setNums(nums.concat('1'))} */>1</button>
+          <button onClick={() => setNums(`${nums}${2}`)}>
+          <sup className="sup-number">2</sup> abc
+          </button>
+          <button onClick={() => setNums(nums.concat("3"))}>
+          def <sup className="sup-number">3</sup>
+          </button>
+        </div>
+        <div className='grupo2-container'>
+          <button onClick={() => setNums(nums.concat("4"))}>
+            <sup className="sup-number">4</sup> ghi
+          </button>
+          <button onClick={() => setNums(nums.concat("5"))}>
+            <sup className="sup-number">5</sup> jkl
+          </button>
+          <button onClick={() => setNums(nums.concat("6"))}>
+            mno <sup className="sup-number">6</sup>
+          </button>
+        </div>
+        <div className='grupo3-container'>
+          <button onClick={() => setNums(nums.concat("7"))}>
+            <sup className="sup-numeros">7</sup> pqrs
+          </button>
+          <button onClick={() => setNums(nums.concat("8"))}>
+              <sup className="sup-numeros">8</sup> tuv
+          </button>
+          <button onClick={() => setNums(nums.concat("9"))}>
+            wxyz <sup className="sup-numeros">9</sup>
+          </button>
+        </div>
+      </div>    
   );
 }
 
